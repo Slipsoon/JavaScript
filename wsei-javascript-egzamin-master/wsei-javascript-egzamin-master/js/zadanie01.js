b002df14a7bc349dcd67157062e8d3f68e6f21bf
Sebastian Ślipek <sebastian.slipek@microsoft.wsei.edu.pl>
2020-02-17 21:09:06 +0100

    Removed wsei-javascript-egzamin-master Directory

--- wsei-javascript-egzamin-master/wsei-javascript-egzamin-master/js/zadanie01.js
+++
@@ -1,6 +0,0 @@
-function getSqrt(n) {
-    return Math.sqrt(n);
-}
-
-console.log(getSqrt(4));
-console.log(getSqrt(16));
--- wsei-javascript-egzamin-master/wsei-javascript-egzamin-master/js/zadanie05.js
+++
@@ -1,9 +0,0 @@
-const btns = document.querySelectorAll('button');
-const container = document.querySelector('#container');
-
-btns.forEach(btn => {
-    btn.addEventListener('click', () => {
-        container.textContent = btn.dataset.text;
-        // console.log(btn.dataset.text);
-    })
-})
